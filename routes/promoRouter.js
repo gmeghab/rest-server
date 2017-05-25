fc0b2ae6f8a41c20ef5a67be905dfe815c1fb750
George Meghabghab <George Meghabghab>
2017-05-24 21:23:06 -0400

    New rep

---
+++ routes/promoRouter.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var promoRouter = express.Router();
+var mongoose = require('mongoose');
+var Promotions=require('../models/promotions');
+var bodyParser = require('body-parser');
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/promotions');
+
+promoRouter.route('/')
+.get(function(req,res,next){
+    Promotions.find({},function(err,promotion){
+		if (err) throw err;
+		res.json(promotion);
+});
+})
+
+.post(function (req, res, next) {
+    Promotions.create(req.body, function (err, promotion) {
+        if (err) throw err;
+        console.log('Promotion created!');
+        var id = promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the promotion with id: ' + id);
+    });
+})
+.delete(function (req, res, next) {
+    Promotions.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+
+promoRouter.route('/:promotionId')
+.get(function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.put(function (req, res, next) {
+    Promotions.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.delete(function (req, res, next) {
+    Promotions.findByIdAndRemove(req.params.promotionId, function (err, resp) {        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+
+module.exports = promoRouter;
